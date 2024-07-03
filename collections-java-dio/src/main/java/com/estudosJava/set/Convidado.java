3f3e5c0c9d2ab957fc35f8ce26f0d5a0aaff582f
Marcelo <marcelodemoura14@gmail.com>
2024-07-03 19:51:26 -0300

    criado lista de convidados

---
+++ collections-java-dio/src/main/java/com/estudosJava/set/Convidado.java
@@ -0,0 +1,41 @@
+package com.estudosJava.set;
+
+import java.util.Objects;
+
+public class Convidado {
+
+    private String nome;
+    private int nrConvite;
+
+    public Convidado(String nome, int nrConvite) {
+        this.nome = nome;
+        this.nrConvite = nrConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getNrConvite() {
+        return nrConvite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Convidado convidado)) return false;
+        return getNrConvite() == convidado.getNrConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nrConvite);
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", nrConvite=" + nrConvite +
+                '}';
+    }
+}
---
+++ collections-java-dio/src/main/java/com/estudosJava/set/ListaConvidados.java
@@ -0,0 +1,62 @@
+package com.estudosJava.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListaConvidados {
+
+    public Set<Convidado> convidadoSet;
+
+    public ListaConvidados(Set<Convidado> convidadoSet) {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public ListaConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void addConvidado(String nome, int nrConvite) {
+        convidadoSet.add(new Convidado(nome, nrConvite));
+    }
+
+    public void removeNrConvite(int nrConvite) {
+        Convidado removeConvidado = null;
+        for (Convidado convidado : convidadoSet) {
+            if (convidado.getNrConvite() == nrConvite) {
+                removeConvidado = convidado;
+                break;
+            }
+        }
+        convidadoSet.remove(removeConvidado);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidado() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+
+        ListaConvidados listaConvidados = new ListaConvidados();
+
+        listaConvidados.addConvidado("jose", 123);
+        listaConvidados.addConvidado("hjklp", 123);
+        listaConvidados.addConvidado("se", 125);
+        listaConvidados.addConvidado("je", 126);
+        listaConvidados.addConvidado("jjja", 127);
+        listaConvidados.addConvidado("jojo",128);
+        listaConvidados.exibirConvidado();
+
+        System.out.println("existem " + listaConvidados.contarConvidados() + " dentro da lista de convidaos");
+
+
+        listaConvidados.removeNrConvite(123);
+        System.out.println("Existem " + listaConvidados.contarConvidados() + "convidados dentro da lista de convidaos");
+
+        listaConvidados.exibirConvidado();
+
+    }
+}
